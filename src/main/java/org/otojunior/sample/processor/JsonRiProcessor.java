27871ae743560fcab179dd3fb4610eb8f075a44e
Oto Junior <otojunior@gmail.com>
2015-07-14 09:18:39 -0300

    RI Implementação

--- src/main/java/org/otojunior/sample/processor/JsonRiProcessor.java
+++ src/main/java/org/otojunior/sample/processor/JsonRiProcessor.java
@@ -5,6 +5,8 @@ package org.otojunior.sample.processor;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -15,6 +17,7 @@ import javax.json.JsonValue;
 import org.apache.commons.io.IOUtils;
 import org.otojunior.sample.datastructure.Address;
 import org.otojunior.sample.datastructure.Customer;
+import org.otojunior.sample.datastructure.PhoneNumber;
 
 /**
  * @author 01456231650
@@ -49,17 +52,25 @@ public class JsonRiProcessor {
 		address.setCity(jsonObjectAddr.getString("city"));
 		address.setPostalCode(Integer.valueOf(jsonObjectAddr.getString("postalCode")));
 		address.setState(jsonObjectAddr.getString("state"));
+		address.setStreetAddress(jsonObjectAddr.getString("streetAddress"));
 		
-		JsonArray jsonArray = jsonObject.getJsonArray("phoneNumber");
-		
-				
+		List<PhoneNumber> phoneNumberList = new ArrayList<PhoneNumber>();
+		JsonArray array = jsonObject.getJsonArray("phoneNumber");
+		for (JsonValue jsonValue : array) {
+			JsonObject obj = (JsonObject) jsonValue;
+			PhoneNumber phoneNumber = new PhoneNumber();
+			phoneNumber.setType(obj.getString("type"));
+			phoneNumber.setNumber(obj.getString("number"));
+			phoneNumberList.add(phoneNumber);
+		}
+						
 		Customer customer = new Customer();
 		customer.setFirstName(jsonObject.getString("firstName"));
 		customer.setLastName(jsonObject.getString("lastName"));
 		customer.setAge(Integer.valueOf(jsonObject.getInt("age")));
 		customer.setAddress(address);
+		customer.setPhoneNumber(phoneNumberList);
 		
 		return customer;
 	}
-
 }
