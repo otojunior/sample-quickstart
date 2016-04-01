7dfa38896a342a5667a526ba53c27595a5a8bb54
Oto Junior <otojunior@gmail.com>
2016-04-01 09:43:09 -0300

    Evolução

--- src/main/java/org/otojunior/sample/App.java
+++ src/main/java/org/otojunior/sample/App.java
@@ -1,5 +1,11 @@
 package org.otojunior.sample;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,8 +22,15 @@ public class App {
 	/**
 	 * Application main method.
 	 * @param args Command line arguments.
+	 * @throws IOException 
+	 * @throws UnknownHostException 
 	 */
-	public static void main(String[] args) {
-		LOG.info("sample-quickstart Application.");		
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		LOG.info("sample-quickstart Application.");
+		
+		NTPUDPClient client = new NTPUDPClient();
+		client.setDefaultTimeout(5000);
+		TimeInfo info = client.getTime(InetAddress.getByName("pool.ntp.org"), 123);
+		LOG.info(info.toString());
 	}
 }
