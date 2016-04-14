d0e5dd89d575a151db1e8c4a432b1fe6acc08c37
Oto Junior <otojunior@gmail.com>
2016-04-14 15:12:07 -0300

    Blocking Queue

--- src/main/java/org/otojunior/sample/App.java
+++ src/main/java/org/otojunior/sample/App.java
@@ -1,5 +1,8 @@
 package org.otojunior.sample;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,11 +16,52 @@ public class App {
 	 */
 	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 	
+	public static final BlockingQueue<String> q = new ArrayBlockingQueue<String>(5);
+	
 	/**
 	 * Application main method.
 	 * @param args Command line arguments.
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
-		LOG.info("sample-quickstart Application.");		
+	public static void main(String[] args) throws InterruptedException {
+		LOG.info("sample-quickstart Application.");
+
+		new Producer().start();
+		new Consumer().start();
+		
+		LOG.info("End");
+	}
+
+	/**
+	 * 
+	 * @author 01456231650
+	 *
+	 */
+	public static class Producer extends Thread {
+		@Override
+		public void run() {
+			try {
+				for (int i = 0; i < 20; i++) {
+					App.q.put("element " + i);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static class Consumer extends Thread {
+		@Override
+		public void run() {
+			try {
+				for (int i = 0; i < 20; i++) {
+					String str = App.q.take();
+					System.out.println(str);
+					Thread.sleep(3000);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
