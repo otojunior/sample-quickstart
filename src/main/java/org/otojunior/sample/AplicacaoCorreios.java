0a67273de37ae321587a0c1c3d045f5f50c15512
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2016-06-27 14:21:20 -0300

    Cliente SOAP para WS dos Correios - Consulta SRO

--- src/main/java/org/otojunior/sample/AplicacaoCorreios.java
+++ src/main/java/org/otojunior/sample/AplicacaoCorreios.java
@@ -1,5 +1,10 @@
 package org.otojunior.sample;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.otojunior.sample.soap.AtendeCliente;
 import org.otojunior.sample.soap.AtendeClienteService;
 import org.otojunior.sample.soap.EnderecoERP;
@@ -17,22 +22,47 @@ public class AplicacaoCorreios {
 	 * <p>main.</p>
 	 *
 	 * @param args an array of {@link java.lang.String} objects.
+	 * @throws ParserConfigurationException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		AtendeClienteService service = new AtendeClienteService();
 		AtendeCliente port = service.getAtendeClientePort();
 		
 		try {
-			EnderecoERP endereco = port.consultaCEP("31360280");
-			
-			StringBuilder str = new StringBuilder();
-			str.append(endereco.getEnd()).append("\n").
-				append(endereco.getBairro()).append("\n").
-				append(endereco.getCidade()).append("\n").
-				append(endereco.getUf()).append("\n");
-			System.out.println(str.toString());
+			consultarCEP(port);
+			consultarSRO(port);
 		} catch (SQLException_Exception | SigepClienteException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * @param port
+	 * @throws SigepClienteException
+	 * @throws ParserConfigurationException 
+	 */
+	private static void consultarSRO(AtendeCliente port) throws Exception {
+		List<String> lst = Arrays.asList(
+			"DU511327170BR", 
+			"DU511279868BR", 
+			"DM788604881BR");		
+		String sro = port.consultaSRO(lst, "L", "T", "ECT", "SRO");
+		System.out.println(sro);
+	}
+
+	/**
+	 * @param port
+	 * @throws SQLException_Exception
+	 * @throws SigepClienteException
+	 */
+	private static void consultarCEP(AtendeCliente port) throws SQLException_Exception, SigepClienteException {
+		EnderecoERP endereco = port.consultaCEP("30730440");
+					
+		StringBuilder str = new StringBuilder();
+		str.append(endereco.getEnd()).append("\n").
+			append(endereco.getBairro()).append("\n").
+			append(endereco.getCidade()).append("\n").
+			append(endereco.getUf()).append("\n");
+		System.out.println(str.toString());
 	}
 }
