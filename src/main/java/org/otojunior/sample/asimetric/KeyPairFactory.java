c7c75c30ad9650162db9fe45a41e9958b3e7dfc3
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2016-10-27 17:45:16 -0200

    Implementação de criptografia assimétrica

---
+++ src/main/java/org/otojunior/sample/asimetric/KeyPairFactory.java
@@ -0,0 +1,50 @@
+package org.otojunior.sample.asimetric;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import org.apache.commons.codec.binary.Hex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>Keys class.</p>
+ *
+ * @author Oto Junior
+ * @version $Id: $Id
+ */
+public class KeyPairFactory {
+	private static final Logger LOG = LoggerFactory.getLogger(KeyPairFactory.class);
+	
+	/**
+	 * 
+	 */
+	public static KeyPair generate(String algorithm, int keysize) {
+		KeyPair keys = null;
+		try {
+			SecureRandom rnd = SecureRandom.getInstance("SHA1PRNG");
+			
+			/*
+			 * Key Generation.
+			 */
+			KeyPairGenerator generator = KeyPairGenerator.getInstance(algorithm);
+			generator.initialize(keysize, rnd);
+			keys = generator.generateKeyPair();
+			
+			LOG.info("Private Key: " + 
+				keys.getPrivate().getEncoded().length + " " + 
+				keys.getPrivate().getFormat() + " " + 
+				Hex.encodeHexString(keys.getPrivate().getEncoded()));
+			
+			LOG.info("Private Key: " + 
+				keys.getPublic().getEncoded().length + " " + 
+				keys.getPublic().getFormat() + " " + 
+				Hex.encodeHexString(keys.getPublic().getEncoded()));
+		} catch (NoSuchAlgorithmException e) {
+			LOG.error(e.getMessage(), e);
+		}
+		return keys;
+	}
+}
---
+++ src/main/java/org/otojunior/sample/asimetric/TestApp.java
@@ -0,0 +1,71 @@
+package org.otojunior.sample.asimetric;
+
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+
+import org.apache.commons.codec.binary.Hex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Application Main Class.
+ *
+ * @author [Author name]
+ * @version $Id: $Id
+ */
+public class TestApp {
+	/**
+	 * SLF4J Logger.
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(TestApp.class);
+	
+	private static final String STR_MESSAGE = "Não obstante, a contínua expansão de nossa atividade "
+			+ "pode nos levar a considerar a reestruturação do investimento "
+			+ "em reciclagem técnica. O empenho em analisar a adoção de "
+			+ "políticas descentralizadoras facilita a criação do retorno "
+			+ "esperado a longo prazo. Ainda assim, existem dúvidas a respeito "
+			+ "de como o surgimento do comércio virtual prepara-nos para "
+			+ "enfrentar situações atípicas decorrentes do fluxo de informações.";
+	
+	private static final byte[] ORIGINAL_MESSAGE = STR_MESSAGE.getBytes();
+	
+	/**
+	 * Application main method.
+	 *
+	 * @param args Command line arguments.
+	 * @throws java.security.NoSuchAlgorithmException if any.
+	 */
+	public static void main(String[] args) {
+		LOG.info("sample-quickstart Application.");
+		
+		test("RSA", 4096, "RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+	}
+	
+	/**
+	 * 
+	 * @param keygenAlgorithm
+	 * @param keySize
+	 * @param cipherAlgorithm
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static void test(String keygenAlgorithm, int keySize, String cipherAlgorithm) {
+		KeyPair keys = KeyPairFactory.generate(keygenAlgorithm, keySize);
+		
+		BlockCipher cipher = new BlockCipher(cipherAlgorithm);
+		byte[] encrypted = cipher.process(ORIGINAL_MESSAGE, keys.getPublic(), Cipher.ENCRYPT_MODE);
+		
+		LOG.info("Message: " + ORIGINAL_MESSAGE.length + " " + Hex.encodeHexString(ORIGINAL_MESSAGE));
+		LOG.info("Encypted: " + encrypted.length + " " + Hex.encodeHexString(encrypted));
+		
+		cipher = new BlockCipher(cipherAlgorithm);
+		byte[] decrypted = cipher.process(encrypted, keys.getPrivate(), Cipher.DECRYPT_MODE);
+		
+		LOG.info("Decrypted: " + decrypted.length + " " + Hex.encodeHexString(decrypted));
+		LOG.info("Original equals Decrypted? " + Arrays.equals(ORIGINAL_MESSAGE, decrypted));
+		
+		LOG.info("------------------------------------------------------------------------------");
+	}
+}
