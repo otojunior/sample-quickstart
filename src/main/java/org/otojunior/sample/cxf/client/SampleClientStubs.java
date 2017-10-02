1d526c8bd48f3fad9c01e9b0c8c4e8d5c05da5ea
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2017-10-02 13:12:28 -0300

    CXF Client com Stub

---
+++ src/main/java/org/otojunior/sample/cxf/client/SampleClientStubs.java
@@ -0,0 +1,23 @@
+package org.otojunior.sample.cxf.client;
+
+import org.otojunior.sample.cxf.entidade.Calculadora;
+import org.otojunior.sample.cxf.entidade.CalculadoraService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author 01456231650
+ *
+ */
+public class SampleClientStubs {
+	private static final Logger LOG = LoggerFactory.getLogger(SampleClientStubs.class);
+	
+	public static void main(String[] args) {
+		CalculadoraService service = new CalculadoraService();
+		Calculadora proxy = service.getCalculadoraPort();
+		
+		LOG.info(proxy.somar(10, 7).toString());
+		LOG.info(proxy.subtrair(10, 7).toString());
+	}
+}
