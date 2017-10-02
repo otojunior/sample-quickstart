885eb0d8f4bc8b515aa721a57e238cfc1874f2df
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2017-10-02 13:31:12 -0300

    Corrigindo alguns parâmetros do webservice

--- src/main/java/org/otojunior/sample/cxf/entidade/Calculadora.java
+++ src/main/java/org/otojunior/sample/cxf/entidade/Calculadora.java
@@ -4,22 +4,25 @@
 package org.otojunior.sample.cxf.entidade;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 /**
  * @author 01456231650
  *
  */
-@WebService()
+@WebService
 public class Calculadora implements ICalculadora {
 	private static final long serialVersionUID = 1L;
 
-	/* (non-Javadoc)
-	 * @see org.otojunior.sample.cxf.entidade.ICalculadora#somar(java.lang.Integer, java.lang.Integer)
+	/**
+	 * 
 	 */
-	@WebMethod
 	@Override
-	public Integer somar(Integer numero1, Integer numero2) {
+	@WebMethod
+	public Integer somar(
+			@WebParam(name="numeroA") Integer numero1, 
+			@WebParam(name="numeroB") Integer numero2) {
 		Integer result = Integer.valueOf(
 			numero1.intValue() + 
 			numero2.intValue());
@@ -29,9 +32,11 @@ public class Calculadora implements ICalculadora {
 	/* (non-Javadoc)
 	 * @see org.otojunior.sample.cxf.entidade.ICalculadora#subtrair(java.lang.Integer, java.lang.Integer)
 	 */
-	@WebMethod
 	@Override
-	public Integer subtrair(Integer numero1, Integer numero2) {
+	@WebMethod
+	public Integer subtrair(
+			@WebParam(name="numeroA") Integer numero1, 
+			@WebParam(name="numeroB") Integer numero2) {
 		Integer result = Integer.valueOf(
 			numero1.intValue() - 
 			numero2.intValue());
@@ -41,17 +46,21 @@ public class Calculadora implements ICalculadora {
 	/* (non-Javadoc)
 	 * @see org.otojunior.sample.cxf.entidade.ICalculadora#multiplicar(java.lang.Integer, java.lang.Integer)
 	 */
-	@WebMethod
 	@Override
-	public Integer multiplicar(Integer numero1, Integer numero2) {
+	@WebMethod
+	public Integer multiplicar(
+			@WebParam(name="numeroA") Integer numero1, 
+			@WebParam(name="numeroB") Integer numero2) {
 		Integer result = Integer.valueOf(
 			numero1.intValue() * 
 			numero2.intValue());
 		return result;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.otojunior.sample.cxf.entidade.ICalculadora#dividir(java.lang.Integer, java.lang.Integer)
+	/*
+	 * Por padrão, uma classe anotada com @WebService, 
+	 * os métodos serão expostos com @WebMethod
+	 * automaticamente.
 	 */
 	@Override
 	public Double dividir(Integer numero1, Integer numero2) {
